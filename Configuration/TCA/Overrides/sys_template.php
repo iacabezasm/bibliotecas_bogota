df359d38f3d6a43510cb07fc15f3c64c901e0a28
iacabezasm <iacabezasm@unal.edu.co>
2022-01-19 20:06:26 -0500

    actualización descripción template

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,3 +1,3 @@
 <?php
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('bibliotecas_bogota', 'Configuration/TypoScript', 'Plantilla web del Sistema Nacional de Bibliotecas Universidad Nacional de Colombia');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('bibliotecas_bogota', 'Configuration/TypoScript', 'Plantilla web de la División de Bibliotecas Universidad Nacional de Colombia');
